886df42ec49bea96c1cb7977c3f103183d800c4f
tejasnat <96818791+tejasnat@users.noreply.github.com>
2026-07-19 15:57:06 +0530

    Prepare portfolio for tejasnat.github.io

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/tejasnatekar",
-  assetPrefix: "/tejasnatekar",
   trailingSlash: true,
   images: { unoptimized: true },
 };
